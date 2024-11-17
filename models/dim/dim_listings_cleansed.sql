2ecac508901ae0362ea86b0a820f8c00f29f5377
dirumisra <dhirajk266@gmail.com>
2024-11-18 03:02:15 +0530

    initial commit

---
+++ models/dim/dim_listings_cleansed.sql
@@ -0,0 +1,26 @@
+WITH src_listings AS (
+    SELECT
+        *
+    FROM
+        {{ ref('src_listings') }}
+)
+
+SELECT
+    listing_id,
+    listing_name,
+    room_type,
+    -- Conditional column: handle zero minimum nights
+    CASE
+        WHEN minimum_nights = 0 THEN 1
+        ELSE minimum_nights
+    END AS minimum_nights,
+
+    host_id,
+
+    -- Remove dollar sign and convert to numeric with precision 10,2
+    CAST(REPLACE(price_str, '$', '') AS NUMBER(10, 2)) AS price,
+
+    created_at,
+    updated_at
+FROM
+    src_listings;
---
+++ models/src/src_hosts.sql
@@ -0,0 +1,13 @@
+WITH raw_hosts as
+(
+    SELECT * 
+    FROM AIRBNB.RAW.RAW_HOSTS
+)
+
+SELECT 
+    id as host_id,
+    NAME as host_name,
+    is_superhost,
+    created_at,
+    updated_at
+FROM raw_hosts
